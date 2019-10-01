2b0bfe9efec589e69ab6b4586c71b060ce539772
ahiodette <ahiode6@gmail.com>
2019-10-01 15:45:45 +0200

    Define save method

--- src/main/java/models/Lands.java
+++ src/main/java/models/Lands.java
@@ -1,17 +1,52 @@
 package models;
 
-public class Lands {
-    private String location;
-    private String description;
-    private String price;
-    private String purpose;
-    private int plotNumber;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-    public Lands(String location, String description, String price, int plotNumber) {
+import javax.swing.*;
+import java.util.List;
+
+
+public class Lands {
+    private String name;
+    private String email;
+    private String property;
+    private String location;
+    private String meansOfPayment;
+    private int price;
+    private int id;
+
+    public Lands(String name, String email, String property, String location, String meansOfPayment, int price) {
+        this.name = name;
+        this.email = email;
+        this.property = property;
         this.location = location;
-        this.description = description;
+        this.meansOfPayment = meansOfPayment;
         this.price = price;
-        this.plotNumber = plotNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
     }
 
     public String getLocation() {
@@ -22,28 +57,55 @@ public class Lands {
         this.location = location;
     }
 
-    public String getDescription() {
-        return description;
+    public String getMeansOfPayment() {
+        return meansOfPayment;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setMeansOfPayment(String meansOfPayment) {
+        this.meansOfPayment = meansOfPayment;
     }
 
-    public String getPrice() {
+    public int getPrice() {
         return price;
     }
 
-    public void setPrice(String price) {
+    public void setPrice(int price) {
         this.price = price;
     }
 
-    public int getPlotNumber() {
-        return plotNumber;
+    public int getId() {
+        return id;
     }
 
-    public void setPlotNumber(int plotNumber) {
-        this.plotNumber = plotNumber;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void save(){
+        try (Connection con =  DB.sql2o.open()){
+            String sql = "INSERT INTO lands (name, email, property, location, meansOfPayment)";
+            this.id =(int) con.createQuery(sql, true)
+                .addParameter("name", this.name)
+                    .addParameter("email", this.email)
+                    .addParameter("property", property)
+                    .addParameter("location", this.location)
+                    .addParameter("meansOfPayment", this.meansOfPayment )
+                    .executeUpdate()
+                    .getKey();
+        }
+
+    }
+
+    public static List<Lands> allLands(){
+        String sql = "SELECT * FROM lands;";
+        try (Connection con = DB.sql2o.open()){
+
+            return con.createQuery(sql).executeAndFetch(Lands.class);
+
+        }
     }
 }
+
+
+
 
